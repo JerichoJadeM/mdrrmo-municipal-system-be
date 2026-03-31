8ca243eb1c2c2de30336e4dbf536e880ee403fef
Jericho Jade Madolid <saserdote.jerichojade@gmail.com>
2026-03-31 17:17:07 +0800

    config: added docker file for render

---
+++ src/main/java/com/isufst/mdrrmosystem/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.isufst.mdrrmosystem.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
