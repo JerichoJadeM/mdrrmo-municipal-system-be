ec70fdfa116fa1031f5c0f1cff2845fe22993567
Jericho Jade Madolid <saserdote.jerichojade@gmail.com>
2026-03-31 17:02:50 +0800

    config: set configuration for db remote setup using aiven

--- src/main/java/com/isufst/mdrrmosystem/entity/Authority.java
+++ src/main/java/com/isufst/mdrrmosystem/entity/Authority.java
@@ -1,5 +1,6 @@
 package com.isufst.mdrrmosystem.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
@@ -7,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 @Embeddable
 public class Authority implements GrantedAuthority {
 
+    @Column(name = "authority", nullable = false)
     private String authority;
 
     public Authority () {}
